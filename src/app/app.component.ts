bb718aca0f5e9922330c824c2b7d3681c825511a
Benbdlt <bordelet.benjamin@free.fr>
2023-12-07 10:05:18 +0100

    Architecture-micro-services-front Init project & layouts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {LayoutsComponent} from "./layouts/layouts.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LayoutsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
